0d2b8552d9e2bbe68f145d08d711231bbf57c77a
An Nguyen <annguyen.it.utc@gmail.com>
2024-08-21 22:27:07 +0700

    feat: `idName` model

---
+++ src/Common/Models/IdNameModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Utconnect.Common.Models;
+
+public class IdNameModel<TId, TName>
+{
+    public required TId Id { get; set; }
+    public required TName Name { get; set; }
+}
+
+public class IdNameModel : IdNameModel<string, string>;
