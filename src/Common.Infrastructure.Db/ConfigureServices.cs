9f887dcb6b361f67223bd9025ef673c3c3922fd8
An Nguyen <annguyen.it.utc@gmail.com>
2024-08-24 23:43:14 +0700

    feat(infrastructure/db): inject `AuditableEntitySaveChangesInterceptor` via method

---
+++ src/Common.Infrastructure.Db/ConfigureServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Utconnect.Common.Infrastructure.Db.Interceptors;
+
+namespace Utconnect.Common.Infrastructure.Db;
+
+public static class ConfigureServices
+{
+    public static void AddAuditableEntityInterceptor(this IServiceCollection services)
+    {
+        services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+    }
+}
