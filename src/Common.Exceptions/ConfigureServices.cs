196afa3e10de83f0678afecc5ee693c68c79f878
An Nguyen <annguyen.it.utc@gmail.com>
2024-08-24 23:42:46 +0700

    feat(exception): inject `HttpResponseExceptionFilter` via method

---
+++ src/Common.Exceptions/ConfigureServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Utconnect.Common.Exceptions.Filters;
+
+namespace Utconnect.Common.Exceptions;
+
+/// <summary>
+/// Provides extension methods for configuring common services.
+/// </summary>
+public static class ConfigureServices
+{
+    /// <summary>
+    /// Registers common services with the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to which the services will be added.</param>
+    public static void AddExceptionFilter(this IServiceCollection services)
+    {
+        services.AddScoped<HttpResponseExceptionFilter>();
+    }
+}
