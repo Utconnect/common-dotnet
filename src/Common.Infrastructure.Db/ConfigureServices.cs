092fa48a3417a19ad1735bb4da9a8e41c0fa9e4a
An Nguyen <annguyen.it.utc@gmail.com>
2024-08-31 23:13:26 +0700

    feat(infrastructure/db): unit of work

--- src/Common.Infrastructure.Db/ConfigureServices.cs
+++ src/Common.Infrastructure.Db/ConfigureServices.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Utconnect.Common.Infrastructure.Db.Interceptors;
+using Utconnect.Common.Infrastructure.Db.Persistence;
 
 namespace Utconnect.Common.Infrastructure.Db
 {
     public static class ConfigureServices
     {
+        public static void AddUnitOfWork<T>(this IServiceCollection services) where T : class, IUnitOfWork
+        {
+            services.AddScoped<IUnitOfWork, T>();
+        }
+
         public static void AddAuditableEntityInterceptor(this IServiceCollection services)
         {
             services.AddScoped<AuditableEntitySaveChangesInterceptor>();
---
+++ src/Common.Infrastructure.Db/Persistence/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Utconnect.Common.Infrastructure.Db.Persistence
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+    }
+}
